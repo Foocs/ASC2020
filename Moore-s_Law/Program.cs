242df5b033f16511fccdd26178fb987d58b0c2b6
Andrei Kovacs <andreijok678@yahoo.com>
2020-10-08 17:00:13 +0300

    fixed logarithm

--- Moore-s_Law/Program.cs
+++ Moore-s_Law/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Moore_s_Law
-{   
+{
     class Program
     {
         static void Main(string[] args)
         {
-            // <!important> Dupa 7 ani vom avea o putere de calcul de 100 de ori mai mare  </important>
+            Console.WriteLine(Math.Log(100,2));
+            // <!important> Dupa 10.5 ani vom avea o putere de calcul de 100 de ori mai mare  </important>
             string s;
             Console.WriteLine("Introduceti un numar n pentru a afla in cati ani vom avea o putere de calcul de n ori mai mare fate de cat avem in acest moment:");
             s = Console.ReadLine();
@@ -21,8 +18,8 @@ namespace Moore_s_Law
                     int n;
                     double x;
                     n = int.Parse(s);
-                    x = Math.Log(n) * 1.5d;
-                    //x = Math.Round(x);
+                    x = Math.Log(n, 2) * 1.5d;
+                    x = Math.Round(x, 2);
                     Console.WriteLine($"Dupa aproximativ {x} ani vom avea o putere de calcul de {n} ori mai mare la acelasi pret.");
                 }
                 catch (FormatException e)
