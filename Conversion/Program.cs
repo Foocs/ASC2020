eb43011f640f09233f0112e04fcc7a16c887ccc6
Andrei Kovacs <andreijok678@yahoo.com>
2020-10-20 23:15:45 +0300

    improved readability

--- Conversion/Program.cs
+++ Conversion/Program.cs
@@ -13,10 +13,6 @@ namespace Conversion
             return ((int)(nr * bazaTinta)).ToString() + convertFromBase10Decimal(nr * bazaTinta - Math.Floor(nr * bazaTinta), bazaTinta, nrZecimale + 1);
         }
 
-        static int val(string nr, int index)
-        {
-            return nr[nr.Length - index - 1];
-        }
         static string convertFromBase10Whole(int nr, int bazaTinta) // convertim in baza 10 prin impartiri repetate, concatenand restul
         {
             if (nr != 0)
@@ -30,16 +26,21 @@ namespace Conversion
             return "";
         }
 
+        static int valAtIndex(string nr, int index)
+        {
+            return nr[nr.Length - index - 1];
+        }
+
         static double convertToBase10(string nr, int bazaOriginala, int nrZecimale, int index = 0) // convertim din baza 10 prin formula numar[index] * bazaTinta ^ index
         {
             if (index < nr.Length)
             {
-                if (nr[nr.Length - index - 1] >= '0' && nr[nr.Length - index - 1] <= '9')
+                if (valAtIndex(nr, index) >= '0' && valAtIndex(nr, index) <= '9')
                     return convertToBase10(nr, bazaOriginala, nrZecimale, index + 1) +
-                           (val(nr, index) - '0') * Math.Pow(bazaOriginala, index - nrZecimale); // convertirea din cifre
+                           (valAtIndex(nr, index) - '0') * Math.Pow(bazaOriginala, index - nrZecimale); // convertirea din cifre
 
                 return convertToBase10(nr, bazaOriginala, nrZecimale, index + 1) +
-                      (val(nr, index) - 'A' + 10) * Math.Pow(bazaOriginala, index - nrZecimale); // convertirea din litere
+                      (valAtIndex(nr, index) - 'A' + 10) * Math.Pow(bazaOriginala, index - nrZecimale); // convertirea din litere
             }
             return 0;
         }
@@ -81,28 +82,29 @@ namespace Conversion
 
                     #endregion
 
-                    #region Format for decimal/minus sign
+                    #region Formatting
+
                     bool minus = nr.IndexOf('-') == -1 ? false : true; // verificam daca numarul este negativ
                     nr = nr.Replace("-", "");                          // eliminam minusul
 
-                    int nrZecimale = nr.IndexOf('.') == -1 ? nr.IndexOf(',') == -1 ? nr.Length - 1 : nr.IndexOf(',') : nr.IndexOf('.'); // cautam unde se gasesc cifrele zecimale
-                    nrZecimale = nr.Length - 1 - nrZecimale;                                                                            // calculam cate cifre zecimale sunt
+                    nr = nr.Replace(",", ".");                 // facem ca ambele semne ( "," si ".") sa functioneze
+                    int nrZecimale = 0;
+                    if (nr.IndexOf('.') != -1)
+                        nrZecimale = nr.Split('.')[1].Length;  // impartim numarul in parte intreaga si zecimala iar apoi numaram cate nr zecimale sunt
+                    nr = nr.Replace(".", "");                  //eliminam virgula pentru ca numarul sa poata fi procesat
 
-                    nr = nr.Replace(".", "");
-                    nr = nr.Replace(",", "");
                     #endregion
 
                     #region Custom Exception Handling
 
                     foreach (char c in nr)
                     {
-                        if (!((c >= '0' && c <= '9') ||
-                              (c >= 'A' && c <= 'Z')))    // verificam daca numarul este format din litere si/sau cifre
+                        if (!(char.IsDigit(c) || char.IsLetter(c)))    // verificam daca numarul este format din litere si/sau cifre
                             throw new Exception("\n      Numarul poate avea doar litere si/sau cifre.");
 
                         if (!((c <= '0' + Convert.ToChar(bazaOriginala)) ||
                              ((c <= 'A' + Convert.ToChar(bazaOriginala) - 10))))     // verificam daca numarul este conform bazei originale (daca este corect)
-                            throw new Exception("\n      ERROR: Numerele pot avea doar litere sau/si cifre mai mici ca baza originala.");
+                            throw new Exception("\n      Numerele pot avea doar litere sau/si cifre mai mici ca baza originala.");
                     }
 
                     if ((bazaOriginala < 2 || bazaOriginala > 36) || (bazaTinta < 2 || bazaTinta > 36))
@@ -110,14 +112,18 @@ namespace Conversion
 
                     #endregion
 
+                    #region Conversion
+
                     //convertim numarul in baza 10 apoi in din baza 10 in baza tinta
-                    double nrBaza10 = convertToBase10(nr, bazaOriginala, nrZecimale); // * Math.Pow(bazaTinta, 16); // inmultim pentru a trata cifrele zecimale, afiseaza 16 cifre zecimale
-                    string nrBazaNoua = (int)nrBaza10 != 0 ? convertFromBase10Whole((int)Math.Floor(nrBaza10), bazaTinta) : "0"; // convertim partea intreaga in baza noua, 
-                                                                                                                                 //atasam "0" pentru ca pt [0.x] functia nu returneza nimic
+                    double nrBaza10 = convertToBase10(nr, bazaOriginala, nrZecimale);
+                    string nrBazaNoua = (int)nrBaza10 != 0 ? convertFromBase10Whole((int)nrBaza10, bazaTinta) : "0"; // convertim partea intreaga in baza noua, si atasam "0" in cazul in care partea intreaga e 0
+
                     if (nrZecimale > 0)
                         nrBazaNoua += "." + convertFromBase10Decimal(nrBaza10 - Math.Floor(nrBaza10), bazaTinta); // adaugam zecimalele
 
                     nrBazaNoua = (minus ? "-" : "") + nrBazaNoua; // adaugam minusul (daca este cazul)
+
+                    #endregion
 
                     Console.WriteLine($"\n      Numarul {originalNumber} din baza {bazaOriginala} este egal cu {nrBazaNoua} in baza {bazaTinta}.\n");
                 }
