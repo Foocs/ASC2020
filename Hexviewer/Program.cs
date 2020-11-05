6bce8cc4c8a76208bc7f9980462b32fd66c819c2
Andrei Kovacs <andreijok678@yahoo.com>
2020-11-05 16:10:20 +0200

    Update Code Review

--- Hexviewer/Program.cs
+++ Hexviewer/Program.cs
@@ -25,25 +25,32 @@ namespace Hexviewer
                     char[] caractereDeEliminat = new char[] { ' ', '"' };
                     path = path.Trim(caractereDeEliminat);  // eliminam caracterele speciale de la inceputul si sfarsitul caii (path-ului)
 
+
+                    if (File.Exists(path))
+                        throw new FileNotFoundException("Path gresit.");
+
                     FileStream file = new FileStream(path, FileMode.Open); // variabila prin care vom putea citi din fisier
 
                     byte[] byteBlock = new byte[nrOcteti]; // variabila cu care vom retine textul din fisier pentru a-l prelucra
                     int idx = 0;    // index-ul pentru rand-ul afisat
 
-                    while (file.Read(byteBlock, 0, nrOcteti) > 0)    // citim 16 caractere ca si bytes pana cand nu mai exista nimic in fisier
-                    {
-                        string hex = BitConverter.ToString(byteBlock);  // convertim secventa de bytes intr-un sir hexazecimal
+                    int actual;
 
-                        hex = hex.Replace("-", " ");  // bitconverter separa fiecare bit prin '-', iar noi il eliminam pentru lizibilitate
+                    while ((actual = file.Read(byteBlock, 0, nrOcteti)) > 0)    // citim 16 caractere ca si bytes pana cand nu mai exista nimic in fisier
+                    {
+                        string hex = BitConverter.ToString(byteBlock, 0, actual);  // convertim secventa de bytes intr-un sir hexazecimal
+
+                        //hex = hex.Replace("-", " ");  // bitconverter separa fiecare byte prin '-', iar noi il eliminam pentru lizibilitate
 
                         string text = "";           // string-ul care va contine ce caractere contine defapt textul
-                        for (int i = 0; i < byteBlock.Length; i++)
+                        for (int i = 0; i < actual; i++)
                             text += byteBlock[i] < ' ' ? "." : ((char)byteBlock[i]).ToString();
                         // transformam fiecare byte in caracter iar caracterele speciale (ex. carriage return, newline) (daca exista) in '.'
 
-                        Console.WriteLine($" {idx++:X7}0 : {hex}  | {text}");     // afisam un rand
+                        Console.WriteLine($" {idx:X8} : {hex.PadRight(nrOcteti * 3 - 1)}  | {text}");     // afisam un rand
+                        idx += nrOcteti;
 
-                        Array.Clear(byteBlock, 0, byteBlock.Length);  // golim vectorul de bytes pentru a evita output gresit la ultima linie (ar afisa o parte din linia anterioara)
+                        //Array.Clear(byteBlock, 0, byteBlock.Length);  // golim vectorul de bytes pentru a evita output gresit la ultima linie (ar afisa o parte din linia anterioara)
                     }
 
                     file.Close();
