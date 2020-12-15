f7a20c0c2ffba2fa6c7891c404c0470c23fffad7
Andrei Kovacs <andreijok678@yahoo.com>
2020-12-15 19:44:15 +0200

    🚜 Refactoring

--- Vending Machine/Program.cs
+++ Vending Machine/Program.cs
@@ -3,187 +3,228 @@ using System.Collections.Generic;
 
 namespace Vending_Machine
 {
-    class Program
+
+    #region Context
+
+    class Context
     {
-        #region Context
-
-        class Context
+        State _state = null;
+        internal static int balance { get; set; }
+        internal Context(State state)
         {
-            private State _state = null;
-
-            public Context(State state)
-            {
-                TransitionTo(state);
-            }
-
-            public void TransitionTo(State state)
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("  Current Balance: ");
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine($"{balance / 100f:C}");
-
-                Console.ResetColor();
-
-                _state = state;
-                _state.SetContext(this);
-                Input(this);
-            }
-            public void NickelRequest()
-            {
-                balance += 5;
-                _state.NickelHandle();
-            }
-
-            public void DimeRequest()
-            {
-                balance += 10;
-                _state.DimeHandle();
-            }
-
-            public void QuarterRequest()
-            {
-                balance += 25;
-                _state.QuarterHandle();
-            }
+            TransitionTo(state);
         }
 
-        #endregion
-
-        #region States
-        abstract class State
+        internal void TransitionTo(State state)
         {
-            protected Context _context;
+            Output.CurrentBalance();
 
-            public void SetContext(Context context)
-            {
-                _context = context;
-            }
-
-            public abstract void NickelHandle();
-            public abstract void DimeHandle();
-            public abstract void QuarterHandle();
-
+            _state = state;
+            _state.SetContext(this);
+            Input.GetCoin(this);
+        }
+        internal void NickelRequest()
+        {
+            balance += 5;
+            _state.NickelHandle();
         }
 
-        class ConcreteStateA : State
+        internal void DimeRequest()
         {
-            public override void NickelHandle()
-            {
-                _context.TransitionTo(new ConcreteStateB());
-            }
-            public override void DimeHandle()
-            {
-                _context.TransitionTo(new ConcreteStateC());
-            }
-            public override void QuarterHandle()
-            {
-                Output.ReturnNickel();
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
-
-        }
-        class ConcreteStateB : State
-        {
-            public override void NickelHandle()
-            {
-                _context.TransitionTo(new ConcreteStateC());
-            }
-            public override void DimeHandle()
-            {
-                _context.TransitionTo(new ConcreteStateD());
-            }
-            public override void QuarterHandle()
-            {
-                Output.ReturnDime();
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
-        }
-        class ConcreteStateC : State
-        {
-            public override void NickelHandle()
-            {
-                _context.TransitionTo(new ConcreteStateD());
-            }
-            public override void DimeHandle()
-            {
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
-            public override void QuarterHandle()
-            {
-                Output.ReturnDime();
-                Output.ReturnNickel();
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
-        }
-        class ConcreteStateD : State
-        {
-            public override void NickelHandle()
-            {
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
-            public override void DimeHandle()
-            {
-                Output.ReturnNickel();
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
-            public override void QuarterHandle()
-            {
-                Output.ReturnDime();
-                Output.ReturnDime();
-                Output.DispenseMerchandise();
-
-                _context.TransitionTo(new ConcreteStateA());
-            }
+            balance += 10;
+            _state.DimeHandle();
         }
 
-        #endregion
-
-        #region I/O
-        static class Output
+        internal void QuarterRequest()
         {
-            static public void DispenseMerchandise()
-            {
-                balance = 0;
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("  Merchandise dispensed.");
-                Console.WriteLine();
-                Console.WriteLine();
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("  Try another merchandise.");
-
-                Console.ResetColor();
-            }
-
-            static public void ReturnNickel()
-            {
-                Console.WriteLine("  Returned a nickel in change.");
-            }
-            static public void ReturnDime()
-            {
-                Console.WriteLine("  Returned a dime in change.");
-            }
-
+            balance += 25;
+            _state.QuarterHandle();
         }
-        static void Input(Context context)
+    }
+
+    #endregion
+
+    #region States
+    abstract class State
+    {
+        protected Context _context;
+
+        public void SetContext(Context context)
         {
-            Console.WriteLine("  Introduce a N - Nickel, a D - Dime or a Q - Quarter");
+            _context = context;
+        }
+
+        internal abstract void NickelHandle();
+        internal abstract void DimeHandle();
+        internal abstract void QuarterHandle();
+
+    }
+
+    class ConcreteStateA : State
+    {
+        internal override void NickelHandle()
+        {
+            _context.TransitionTo(new ConcreteStateB());
+        }
+        internal override void DimeHandle()
+        {
+            _context.TransitionTo(new ConcreteStateC());
+        }
+        internal override void QuarterHandle()
+        {
+            Output.ReturnNickel();
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+
+    }
+    class ConcreteStateB : State
+    {
+        internal override void NickelHandle()
+        {
+            _context.TransitionTo(new ConcreteStateC());
+        }
+        internal override void DimeHandle()
+        {
+            _context.TransitionTo(new ConcreteStateD());
+        }
+        internal override void QuarterHandle()
+        {
+            Output.ReturnDime();
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+    }
+    class ConcreteStateC : State
+    {
+        internal override void NickelHandle()
+        {
+            _context.TransitionTo(new ConcreteStateD());
+        }
+        internal override void DimeHandle()
+        {
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+        internal override void QuarterHandle()
+        {
+            Output.ReturnDime();
+            Output.ReturnNickel();
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+    }
+    class ConcreteStateD : State
+    {
+        internal override void NickelHandle()
+        {
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+        internal override void DimeHandle()
+        {
+            Output.ReturnNickel();
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+        internal override void QuarterHandle()
+        {
+            Output.ReturnDime();
+            Output.ReturnDime();
+            Output.DispenseMerchandise();
+
+            _context.TransitionTo(new ConcreteStateA());
+        }
+    }
+
+    #endregion
+
+    #region I/O
+    static class Output
+    {
+        static internal void WelcomeMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  Welcome to the Vending Machine!       ");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"     ___      .-""""-.      ___     ");
+            Console.WriteLine(@"     \  ""-.  /      \  .-""  /     ");
+            Console.WriteLine(@"      > -=.\/        \/.=- <        ");
+            Console.WriteLine(@"      > -='/\        /\'=- <        ");
+            Console.WriteLine(@"     /__.-'  \      /  '-.__\       ");
+            Console.WriteLine(@"              '-..-'                ");
+            Console.WriteLine();
+
+            Console.ResetColor();
+        }
+        static internal void CurrentBalance()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("  Current Balance: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"{Context.balance / 100f:C}");
+
+            Console.ResetColor();
+        }
+        static internal void DispenseMerchandise()
+        {
+            Context.balance = 0;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("  Merchandise dispensed.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  Try another merchandise.");
+
+            Console.ResetColor();
+        }
+
+        static internal void ReturnNickel()
+        {
+            Console.WriteLine("  Returned a nickel in change.");
+        }
+        static internal void ReturnDime()
+        {
+            Console.WriteLine("  Returned a dime in change.");
+        }
+    }
+
+    static class Input
+    {
+        static void Message()
+        {
+            Console.Write("  Introduce a ");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("N - Nickel");
+            Console.ResetColor();
+            Console.Write(", a ");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("D - Dime");
+            Console.ResetColor();
+            Console.Write(" or a ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Q - Quarter");
+            Console.ResetColor();
+            Console.WriteLine();
 
             Console.Write("  Input = ");
+        }
+        static internal void GetCoin(Context context)
+        {
+            Message();
+
             char coinType = char.Parse(Console.ReadLine().ToUpper());
 
             while (coinType != 'N' && coinType != 'D' && coinType != 'Q')
@@ -208,39 +249,29 @@ namespace Vending_Machine
 
             requests[coinType]();
         }
-        #endregion
+    }
 
-        #region Driver Code
+    #endregion
 
-        static private int balance = 0;
-        static void Main(string[] args)
+    #region Driver Code
+    class Vending_Machine
+    {
+
+        static internal void Main(string[] args)
         {
 
             try
             {
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("  Welcome to the Vending Machine!       ");
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("     ___      .-\"\"-.      ___         ");
-                Console.WriteLine("     \\  \"-.  /      \\  .-\"  /       ");
-                Console.WriteLine("      > -=.\\/        \\/.=- <          ");
-                Console.WriteLine("      > -='/\\        /\\'=- <           ");
-                Console.WriteLine("     /__.-'  \\      /  '-.__\\         ");
-                Console.WriteLine("              '-..-'                    ");
-                Console.WriteLine();
-
-                Console.ResetColor();
+                Output.WelcomeMessage();
                 var context = new Context(new ConcreteStateA());
             }
 
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"  {e.Message}");
             }
         }
 
-        #endregion
     }
+    #endregion
 }
