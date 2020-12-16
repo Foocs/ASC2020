6388f6fde80e67da979d227900841364582bd4aa
Andrei Kovacs <andreijok678@yahoo.com>
2020-12-16 22:04:35 +0200

    ➕ Added documentation for the ARC Program

---
+++ ARC_Assembler/ARC documentation/addTwoIntegers.asm
@@ -0,0 +1,12 @@
+! This program adds two numbers
+	.begin
+	.org 2048
+prog1:  ld     [x], %r1	        ! Load x into %r1
+        ld     [y], %r2	        ! Load y into %r2
+        addcc  %r1, %r2, %r3    ! %r3 = %r1 + %r2
+        st     %r3, [z]	        ! Store %r3 into z
+        jmpl   %r15 + 4, %r0    ! Return
+x:      15
+y:      9
+z:      0
+       .end
---
+++ ARC_Assembler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace ARC_Assembler
+{
+    static class Program
+    {
+        // searching for ".begin"
+        static int searchBeginning(this string[] lines)
+        {
+            int currentLine = 0;
+
+            while (currentLine < lines.Length && lines[currentLine].Trim() != ".begin")
+                currentLine++;
+
+            return currentLine;
+        }
+
+        // searching for ".org"
+        static int searchStartingAdress(this string[] lines, ref int currentLine)
+        {
+            for (currentLine++; currentLine < lines.Length; currentLine++)
+            {
+                string[] words = lines[currentLine].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                words[0] = words[0].Trim();
+
+                if (words[0] == ".org")
+                    return int.Parse(words[1]);
+            }
+
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            string filePath = @"..\..\ARC documentation\addTwoIntegers.asm";
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int currentLine = lines.searchBeginning();
+
+            int currentMemoryAdress = lines.searchStartingAdress(ref currentLine);
+
+            Console.WriteLine(currentLine + " " + currentMemoryAdress);
+        }
+    }
+}
